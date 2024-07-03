1a3ef4b45e2e70ccd8004fd566a72930f32bd93e
Anjaiah Methuku <anjijava16@users.noreply.github.com>
2024-07-03 19:54:34 -0400

    Create bde_large.py

---
+++ src/bde_large.py
@@ -0,0 +1,31 @@
+from openai import OpenAI
+import os
+
+# How to get your Databricks token: https://docs.databricks.com/en/dev-tools/auth/pat.html
+DATABRICKS_TOKEN = os.environ.get('DATABRICKS_TOKEN')
+# Alternatively in a Databricks notebook you can use this:
+# DATABRICKS_TOKEN = dbutils.notebook.entry_point.getDbutils().notebook().getContext().apiToken().get()
+
+client = OpenAI(
+  api_key=DATABRICKS_TOKEN,
+  base_url="https://adb-3131138635619485.5.azuredatabricks.net/serving-endpoints"
+)
+
+embeddings = client.embeddings.create(
+  input='Your string for the embedding model goes here',
+  model="databricks-bge-large-en"
+)
+
+print(embeddings.data[0].embedding)
+
+
+SELECT ai_query('databricks-bge-large-en',
+    request => "<Please provide your input string here!>")
+
+
+curl \
+  -u token:$DATABRICKS_TOKEN \
+  -X POST \
+  -H "Content-Type: application/json" \
+  -d@data.json \
+  https://adb-3131138635619485.5.azuredatabricks.net/serving-endpoints/databricks-bge-large-en/invocations
